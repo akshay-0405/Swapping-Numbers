ab04ee2bd295361d420543a56d45cb3e77eaf522
akshay-0405 <95869029+akshay-0405@users.noreply.github.com>
2021-12-09 21:05:50 +0530

    Created AK.c
    A Simple C Program Of Swapping Numbers Without Using Temporary Variables.

---
+++ AK.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	//Took two variables of datatype integer.
+	int a,b;
+	//Accepting 1st integer from user.
+	printf("Enter 1st integer:");
+	scanf("%d",&a);
+	//Accepting 2nd integer from user.
+	printf("Enter 2nd Integer:");
+	scanf("%d",&b);
+	//Swapping Them. 
+	printf("After Swapping,1st integer=%d",b);
+	printf("\nAfter Swapping,2nd Integer=%d",a);
+}  
